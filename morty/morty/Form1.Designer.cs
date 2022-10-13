8753cef1239deb77e26ff0ff37b77d8b4b903f45
musacivi <musacivi7@gmail.com>
2022-10-13 23:48:16 +0300

    Sesli asistan
    c# ile geliştirilebilir sesli asistan

---
+++ morty/morty/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace morty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.konuşToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayarlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::morty.Properties.Resources.Untitled;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(249, 283);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::morty.Properties.Resources.record_voice_over_118744;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 283);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.konuşToolStripMenuItem,
+            this.ayarlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(261, 30);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // konuşToolStripMenuItem
+            // 
+            this.konuşToolStripMenuItem.Name = "konuşToolStripMenuItem";
+            this.konuşToolStripMenuItem.Size = new System.Drawing.Size(61, 26);
+            this.konuşToolStripMenuItem.Text = "konuş";
+            this.konuşToolStripMenuItem.Click += new System.EventHandler(this.konuşToolStripMenuItem_Click);
+            // 
+            // ayarlarToolStripMenuItem
+            // 
+            this.ayarlarToolStripMenuItem.Name = "ayarlarToolStripMenuItem";
+            this.ayarlarToolStripMenuItem.Size = new System.Drawing.Size(68, 26);
+            this.ayarlarToolStripMenuItem.Text = "ayarlar";
+            this.ayarlarToolStripMenuItem.Click += new System.EventHandler(this.ayarlarToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dil-Secenegi";
+            this.label1.Click += new System.EventHandler(this.dil_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 122);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 21);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "English";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 354);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "morty";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem konuşToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayarlarToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ morty/morty/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace morty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speechSyn = new SpeechSynthesizer();
+        bool izin;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }   
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            sestanima_ayarları();
+            recoEngine.RecognizeAsync();
+            izin = true;
+        }
+        void sestanima_ayarları()
+        {
+            string[] ihtimaller = { "Hello", "How are you", "Please open League Of Legends"," open mj","open the opera","open the youtube","open the Visual Studio","What time is it","Thank you MJ","Open the C"
+              ,"Open the Unity","Open the game file","Open the game"};
+            Choices seçenekler = new Choices(ihtimaller);
+            Grammar grammar = new Grammar(new GrammarBuilder(seçenekler));
+            recoEngine.LoadGrammar(grammar);
+            recoEngine.SetInputToDefaultAudioDevice();
+            recoEngine.SpeechRecognized += ses_tanıdıgında;
+
+        }
+
+        private void ses_tanıdıgında(object sender, SpeechRecognizedEventArgs e)
+        {
+            speechSyn.SelectVoiceByHints(VoiceGender.Female);
+            if (izin == true)
+            {
+                pictureBox1.Visible = true;
+                izin = false;
+                if(e.Result.Text == "Please open League Of Legends")
+                {
+                    System.Diagnostics.Process.Start("C:\\Riot Games\\League of Legends");
+                    if(radioButton1.Checked == true)
+                    {
+                        speechSyn.SpeakAsync("Okey Sir League Of Legends is opening");
+                    }
+                }
+                if(e.Result.Text == "Hello")
+                {
+                    speechSyn.SpeakAsync("Hello sir How can ı help you"); 
+                }
+                if (e.Result.Text == "open mj")
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\90536\\Desktop\\cemre");
+                    speechSyn.SpeakAsync(" Sir. Who is this beautiful woman");
+
+                }
+                if (e.Result.Text == "open the opera")
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\90536\\AppData\\Local\\Programs\\Opera\\launcher.exe");
+                    speechSyn.SpeakAsync("Okey I opening opera");
+                }
+                if(e.Result.Text == "open the youtube")
+                {
+                    System.Diagnostics.Process.Start("https://www.youtube.com/signin?action_handle_signin=true&authuser=1&next=https%3A%2F%2Fwww.youtube.com%2F&feature=masthead_switcher&skip_identity_prompt=true");
+                    speechSyn.SpeakAsync("Okey Sir Opening Opera");
+                }
+                if(e.Result.Text == "open the Visual Studio")
+                {
+                    System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\Common7\\IDE\\");
+                    speechSyn.SpeakAsync("Yes sir");
+                } 
+                if(e.Result.Text == "What time is it")
+                {
+
+                    speechSyn.SpeakAsync("Time is"+DateTime.Today);
+                }
+                if(e.Result.Text == "Thank you MJ")
+                {
+                    speechSyn.SpeakAsync("You are welcome sir");
+                }
+                if (e.Result.Text == "Open the C")
+                {
+                    System.Diagnostics.Process.Start("C:\\");
+                    speechSyn.SpeakAsync("C is opening");
+                }
+                if(e.Result.Text == "Open the Unity")
+                {
+                    System.Diagnostics.Process.Start("C:\\Program Files\\Unity Hub\\Unity Hub.exe");
+                    
+                    speechSyn.SpeakAsync("Unity is opening");
+                }
+                if (e.Result.Text == "Open the game file")
+                {
+                    System.Diagnostics.Process.Start("C:\\Users\\90536\\Desktop\\New folder (2)");
+                    speechSyn.SpeakAsync("Game file is opening");
+                }
+                if (e.Result.Text == "Open the game")
+                {
+                    System.Diagnostics.Process.Start("C:\\Riot Games\\Riot Client");
+                    speechSyn.SpeakAsync("Valorant is opening");
+                }
+               
+
+            }
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void konuşToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            label1.Visible = false;
+            radioButton1.Visible = false;
+           
+        }
+
+        private void ayarlarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            label1.Visible = true;
+            radioButton1.Visible = true;
+           
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dil_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
